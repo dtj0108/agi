ba61749477e929c6316311410b92f8460fe3f2ab
dtj0108 <dtj0108@gmail.com>
2026-08-16 14:10:10 -0400

    TS migration cleanup: push batch 2/10 (cli reset dist output)

--- entity/dist/cli/commands/reset.js
+++ entity/dist/cli/commands/reset.js
@@ -51,7 +51,6 @@ export default async function reset(args, projectRoot) {
         rl.question('  Are you absolutely sure? (yes/no) ', resolve);
     });
     rl.close();
-    // @ts-expect-error TODO(ts-migration): TS(2339): Property 'toLowerCase' does not exist on type 'unk... Remove this comment to see the full error message
     if (answer2.toLowerCase() !== 'yes') {
         console.log('\n  Reset cancelled.\n');
         process.exit(0);
